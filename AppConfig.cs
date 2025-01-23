16679ccb07ad0e0dc879a04d9e1e4d2ca799c6d0
XJYdemons <2695648907@qq.com>
2025-01-23 23:13:44 +0800

    第一次上传

---
+++ AppConfig.cs
@@ -0,0 +1,40 @@
+﻿
+
+namespace 金铲铲助手
+{
+    internal class AppConfig
+    {
+        public required string HotKey1 { get; set; }
+        public required string HotKey2 { get; set; }
+        public required string HotKey3 { get; set; }
+        public int intervalTime_GetCard { get; set; }
+        public int intervalTime_StoreRefresh { get; set; }
+        public int intervalTime_RefreshStore { get; set; }
+
+        public int countOfMoney_StartRefreshStore { get; set; }
+        public int countOfMoney_StopRefreshStore { get; set; }
+       
+        public int startPoint_CardScreenshotX1 { get; set; }
+        public int startPoint_CardScreenshotX2 { get; set; }
+        public int startPoint_CardScreenshotX3 { get; set; }
+        public int startPoint_CardScreenshotX4 { get; set; }
+        public int startPoint_CardScreenshotX6 { get; set; }
+        public int startPoint_CardScreenshotY6 { get; set; }
+        public int width_CardScreenshot { get; set; }
+        public int height_CardScreenshot { get; set; }
+        public int startPoint_MoneyScreenshotX { get; set; }
+        public int startPoint_MoneyScreenshotY { get; set; }
+        public int width_MoneyScreenshot { get; set; }
+        public int height_MoneyScreenshot { get; set; }
+        public int startPoint_MutationScreenshotX { get; set; }
+        public int startPoint_MutationScreenshotY { get; set; }
+        public int width_MutationScreenshot { get; set; }
+        public int height_MutationScreenshot { get; set; }
+        public int Point_RefreshMutationX { get; set; }
+        public int Point_RefreshMutationY { get; set; }
+        public int Point_EvolutionMutationX { get; set; }
+        public int Point_EvolutionMutationY { get; set; }
+        public int Point_RefreshStoreX { get; set; }
+        public int Point_RefreshStoreY { get; set; }
+    }
+}
---
+++ OCRTools.cs
@@ -0,0 +1,132 @@
+﻿using System.Text.RegularExpressions;
+using PaddleOCRSharp;
+namespace 金铲铲助手
+{
+    public static class OCRTools
+    {
+        
+      static   PaddleOCRSharp.OCRModelConfig config = new PaddleOCRSharp.OCRModelConfig()
+      {
+          det_infer = @"inference\ch_PP-OCRv4_det_infer",
+          cls_infer = @"inference\ch_ppocr_mobile_v2.0_cls_infer",
+          rec_infer = @"inference\ch_PP-OCRv4_rec_infer",
+          //keys = @"inference\ppocr_keys_order.txt"
+          keys = @"inference\ppocr_keys.txt"
+      };
+        //使用默认参数
+        static PaddleOCRSharp.OCRParameter oCRParameter = new PaddleOCRSharp.OCRParameter();
+        //识别结果对象
+      static   PaddleOCRSharp.OCRResult ocrResult = new PaddleOCRSharp.OCRResult();
+        
+      static   PaddleOCRSharp.PaddleOCREngine engine = new PaddleOCRSharp.PaddleOCREngine(config, oCRParameter);
+
+
+
+        //static PaddleOCRSharp.OCRModelConfig config1 = new PaddleOCRSharp.OCRModelConfig()
+        //{
+        //    det_infer = @"inference\ch_PP-OCRv4_det_infer",
+        //    cls_infer= @"inference\ch_ppocr_mobile_v2.0_cls_infer",
+        //    rec_infer= @"inference\ch_PP-OCRv4_rec_infer",
+        //    keys= @"inference\ppocr_keys_order.txt"
+        //};
+
+        //static PaddleOCRSharp.OCRParameter oCRParameter1 = new PaddleOCRSharp.OCRParameter();
+      
+
+        ////识别结果对象
+        //static PaddleOCRSharp.OCRResult ocrResult1 = new PaddleOCRSharp.OCRResult();
+
+        //static PaddleOCRSharp.PaddleOCREngine engine1 = new PaddleOCREngine(config1, oCRParameter1);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="模式">0:识别奕子名称的算法；1：识别异常突变的算法</param>
+        public static string OCRRecognition(int 模式, Bitmap image)
+        {
+            switch (模式)
+            {
+                case 0:
+                    {
+                        ocrResult = engine.DetectText(image);
+                       
+                       return ocrResult.ToString(); 
+                    }
+
+                case 1:
+                    {
+
+
+
+                        //ocrResult1 = engine1.DetectText(image);
+                        //return ocrResult1.ToString();
+                        return "";
+                    }
+                default:
+                    {
+                        return "未识别";
+
+                    }
+
+            }
+
+        }
+       
+        /// <summary>
+        /// OCR识别，仅输出数字，识别金币专用
+        /// </summary>
+        /// <param name="模式"></param>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public static string OCRRecognitionForFigure(int 模式,Bitmap image)
+        {
+            switch (模式)
+            {
+                case 0:
+                    {
+                        ocrResult = engine.DetectText(image);
+                       
+
+                        if (ocrResult != null)
+                        {
+                            // 使用正则表达式仅提取数字
+                            string digitsOnly = Regex.Replace(ocrResult.Text, "[^0-9]", "");
+                            return digitsOnly.ToString();
+                        }
+                        else
+                        {
+                            return "";
+                        }
+                       
+                    }
+
+                case 1:
+                    {
+
+                        //ocrResult1 = engine1.DetectText(image);
+
+                        //if (ocrResult1 != null)
+                        //{
+                        //    // 使用正则表达式仅提取数字
+                        //    string digitsOnly = Regex.Replace(ocrResult1.Text, "[^0-9]", "");
+                        //    return digitsOnly.ToString();
+                        //}
+                        //else
+                        //{
+                        //    return "";
+                        //}
+                        return "";
+                    }
+                default:
+                    {
+                        return "未识别";
+
+                    }
+
+            }
+
+        }
+
+    
+    }
+}

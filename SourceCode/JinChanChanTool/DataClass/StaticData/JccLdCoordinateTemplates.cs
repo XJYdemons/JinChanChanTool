46301fcc5070f58fbaa48d66440cd2adecf3b3bc
Z <1305508372@qq.com>
2026-07-05 20:12:44 +0800

    微调雷电模拟器英雄名识别区域

--- SourceCode/JinChanChanTool/DataClass/StaticData/JccLdCoordinateTemplates.cs
+++ SourceCode/JinChanChanTool/DataClass/StaticData/JccLdCoordinateTemplates.cs
@@ -28,27 +28,27 @@ namespace JinChanChanTool.DataClass.StaticData
         /// <summary>
         /// 第一个英雄名字区域。
         /// </summary>
-        public static readonly AnchorProfile CardSlot1_Name = new(-333.5, -32.5, 135, 35);
+        public static readonly AnchorProfile CardSlot1_Name = new(-330.5, -32.5, 125, 35);
 
         /// <summary>
         /// 第二个英雄名字区域。
         /// </summary>
-        public static readonly AnchorProfile CardSlot2_Name = new(-136, -32.5, 140, 35);
+        public static readonly AnchorProfile CardSlot2_Name = new(-133, -32.5, 130, 35);
 
         /// <summary>
         /// 第三个英雄名字区域。
         /// </summary>
-        public static readonly AnchorProfile CardSlot3_Name = new(64, -32.5, 140, 35);
+        public static readonly AnchorProfile CardSlot3_Name = new(67, -32.5, 130, 35);
 
         /// <summary>
         /// 第四个英雄名字区域。
         /// </summary>
-        public static readonly AnchorProfile CardSlot4_Name = new(266, -32.5, 150, 35);
+        public static readonly AnchorProfile CardSlot4_Name = new(269, -32.5, 140, 35);
 
         /// <summary>
         /// 第五个英雄名字区域。
         /// </summary>
-        public static readonly AnchorProfile CardSlot5_Name = new(465, -32.5, 150, 35);
+        public static readonly AnchorProfile CardSlot5_Name = new(468, -32.5, 140, 35);
 
         // --- 备用区域 ---
 
